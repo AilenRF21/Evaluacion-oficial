b163a47fc043673b7ef80040345864ff598c0a79
Ailen <ailenroldan3040@gmail.com>
2025-03-28 13:13:42 +0000

    Evaluacion

---
+++ evaluacion_fs/index.js
@@ -0,0 +1,9 @@
+function cambiarNombre(nombre) { 
+
+    console.log("Nuevo nombre:", nombre);
+    console.log("Bienvenido" , nombre)
+}
+
+cambiarNombre("Pedro");
+// 'Nuevo nombre: Pedro'
+// 'Bienvenido Pedro'
---
+++ evaluacion_fs/index2.js
@@ -0,0 +1,14 @@
+function cambiarNombre() {
+    let nombre = "Carlos";
+    nombre = "Pedro"; 
+
+    console.log("Nuevo nombre:", nombre);
+
+    console.log("Bienvenido", nombre);
+
+    
+}
+
+cambiarNombre();
+// 'Nuevo nombre: Pedro'
+// 'Bienvenido Pedro'
